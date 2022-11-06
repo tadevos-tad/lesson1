2e2890a3e6f7a2b4c68f0b6ee5ebff28fb80e7c5
tadevos-tad <tadevos.t1.y@tumo.org>
2022-11-06 14:33:06 +0400

    lesson2

--- grassEater.js
+++ grassEater.js
@@ -1,38 +1,26 @@
-class GrassEater {
-    constructor (x, y) {
-        this.x = x;
-        this.y = y;
+class GrassEater extends parent {
+    constructor(x, y, index){
+        super(x, y, index);
         this.energy = 8;
-        this.directions = [];
     }
+   getNewCoordinates() {
+       this.directions = [
+           [this.x - 1, this.y - 1],
+           [this.x, this.y - 1],
+           [this.x + 1, this.y - 1],
+           [this.x - 1, this.y],
+           [this.x + 1, this.y],
+           [this.x - 1, this.y + 1],
+           [this.x, this.y + 1],
+           [this.x + 1, this.y + 1]
+       ];
+   }
+   chooseCell(character) {
+       this.getNewCoordinates();
+       return super.chooseCell(character);
+   }
+       
 
-    getNewCoordinates() {
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x    , this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y    ],
-            [this.x + 1, this.y    ],
-            [this.x - 1, this.y + 1],
-            [this.x    , this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
-    }
-
-    chooseCell(character) {
-        this.getNewCoordinates();
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if(x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length){
-                if (matrix[y][x] == character) {
-                    found.push(this.directions[i]);
-                }
-            }
-        }
-        return found;
-    }
 
     mul() {
         let newCell = random(this.chooseCell(0));
